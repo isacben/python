135660a6b7b1f94a2633b3d5f13cae2cc9f0ca1b
Isaac Benitez <37629156+isacben@users.noreply.github.com>
2019-12-31 14:05:50 -0800

    Initial commit.

---
+++ reveneu.py
@@ -0,0 +1,25 @@
+# Isaac Benitez
+# Customer Success Manager
+# This scripts gets the reveneu invoiced by a user, within a period of time.
+
+import xmlrpc.client
+
+url = ''
+db = ''
+username = ''
+password = ''
+
+start = "12/01/2019"
+end = "12/31/2019"
+
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+uid = common.authenticate(db, username, password, {})
+
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+res = models.execute_kw(db, uid, password, 'account.invoice', 'search_read', 
+    [[["user_id", "ilike", "ibe"], ["date_invoice", ">=", start], ["date_invoice", "<=", end]]],
+    {'fields': ['amount_total_signed']})
+
+total = sum(map(lambda x: float(x['amount_total_signed']), res))
+
+print('%.2f'%total)
