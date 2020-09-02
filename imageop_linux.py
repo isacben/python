9a359a4452cf88e4bf4fc9f4a4f4e7142a951213
Isaac Benitez <37629156+isacben@users.noreply.github.com>
2020-09-01 22:20:08 -0700

    Create imageop_linux.py

---
+++ imageop_linux.py
@@ -0,0 +1,19 @@
+# Image Optimizer
+# Isaac Benitez Sandoval
+
+from PIL import Image
+from os import listdir, mkdir
+from os.path import isfile, join
+from datetime import datetime
+
+path = "/home/isacben/Documents/square guitar/"
+files = [f for f in listdir(path) if isfile(join(path, f))]
+maxwith = 600
+dest_path = path + "optimized_" + datetime.now().strftime("%m-%d-%Y_%H-%M-%S") + "/"
+mkdir(dest_path)
+
+for file in files:
+    foo = Image.open(path + file)
+    ratio = maxwith/foo.size[0]
+    foo = foo.resize((int(foo.size[0]*ratio),int(foo.size[1]*ratio)))
+    foo.save(dest_path + file, optimize=True, quality=75)
